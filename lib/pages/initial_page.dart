ace0d8dfb89b97b8a36df4052972058d807f7eb1
Sameera Wijesekara <sameerawijesekara2001@gmail.com>
2025-02-18 23:29:26 +0530

    welcome page

---
+++ lib/pages/initial_page.dart
@@ -0,0 +1,77 @@
+import 'package:firebaseflutter/components/my_button.dart';
+import 'package:firebaseflutter/pages/signup_page.dart';
+import 'package:flutter/material.dart';
+
+class InitialPage extends StatelessWidget {
+  const InitialPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(right: 20.0, left: 20, top: 20),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: ClipRRect(
+                child: Image.asset(
+                  'assets/NIKE2.png',
+                  width: 300,
+                  height: 300,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Text(
+              'Just Do It',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Center(
+              child: Column(
+                children: [
+                  Text(
+                    "Brand new sneakers and custom kicks",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 15,
+                    ),
+                  ),
+                  Text(
+                    " made with premium quality.",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 60,
+            ),
+            MyButton(
+              text: 'Shop Now',
+              color: Colors.black,
+              width: double.infinity,
+              height: 50,
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SignupPage(),
+                    ));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
